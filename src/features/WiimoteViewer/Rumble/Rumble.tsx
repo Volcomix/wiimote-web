214e596cce452559c215aac83ce5b9a7c72eae44
Volcomix <Volcomix@users.noreply.github.com>
2022-09-18 20:33:27 +0200

    Handle rumble intensity

--- src/features/WiimoteViewer/Rumble/Rumble.tsx
+++ src/features/WiimoteViewer/Rumble/Rumble.tsx
@@ -1,6 +1,7 @@
 import VibrationIcon from '@mui/icons-material/Vibration'
+import Slider from '@mui/material/Slider'
 import cx from 'classnames'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import { Wiimote } from 'services/wiimote'
 
@@ -10,8 +11,11 @@ type RumbleProps = {
   wiimote: Wiimote
 }
 
+const RUMBLE_STEP = 30
+
 const Rumble = ({ wiimote }: RumbleProps) => {
   const [rumble, setRumble] = useState(wiimote.rumble)
+  const intensity = useRef(wiimote.rumble ? 100 : 0)
 
   useEffect(() => {
     if (!rumble) {
@@ -19,18 +23,41 @@ const Rumble = ({ wiimote }: RumbleProps) => {
     }
     const intervalId = setInterval(() => {
       wiimote.sendRumble(true)
-      setTimeout(() => wiimote.sendRumble(false), 10)
-    }, 30)
-    return () => clearInterval(intervalId)
+      if (intensity.current < 100) {
+        setTimeout(
+          () => wiimote.sendRumble(false),
+          RUMBLE_STEP * (intensity.current / 100)
+        )
+      }
+    }, RUMBLE_STEP)
+    return () => {
+      clearInterval(intervalId)
+      wiimote.sendRumble(false)
+    }
   }, [rumble, wiimote])
 
   return (
-    <button
-      className={cx(styles.root, { [styles.enabled]: rumble })}
-      onClick={() => setRumble(!rumble)}
-    >
-      <VibrationIcon className={styles.icon} />
-    </button>
+    <>
+      <button
+        className={cx(styles.root, { [styles.enabled]: rumble })}
+        onClick={() => setRumble(!rumble)}
+      >
+        <VibrationIcon className={styles.icon} />
+      </button>
+      <Slider
+        className={styles.slider}
+        orientation="vertical"
+        defaultValue={0}
+        onChange={(_, value) => {
+          intensity.current = value as number
+          if (value === 0) {
+            setRumble(false)
+          } else {
+            setRumble(true)
+          }
+        }}
+      />
+    </>
   )
 }
 
